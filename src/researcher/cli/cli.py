ec8db9c6d0e9b50114680e5a7c94a44926dc287e
mortezai <morteza2006g@gmail.com>
2025-07-31 22:40:33 +0330

    feat(cli): add fetch papers to cmd

--- src/researcher/cli/cli.py
+++ src/researcher/cli/cli.py
@@ -1,5 +1,6 @@
 import argparse
 from researcher import ArxivSearch, ArxivDownloader
+from researcher.pipelines import FetchPapersPipeline
 from pathlib import Path
 
 def handle_search(args):
@@ -11,6 +12,11 @@ def handle_download(args):
     downloader = ArxivDownloader(Path(args.output))
     success = downloader.download_pdf(args.url)
     print("Download successful" if success else "Download failed")
+
+def handle_fetch_papers(args):
+    pipeline = FetchPapersPipeline(args.query, args.max_results, Path(args.output))
+    pipeline.run_pipeline()
+    print(f"Papers has been stored in {Path(args.output)} successfully!.")
 
 def main():
     parser = argparse.ArgumentParser(prog="researcher", description="Researcher CLI")
@@ -25,6 +31,12 @@ def main():
     download_parser.add_argument("url", type=str, help="PDF URL")
     download_parser.add_argument("--output", type=str, default=Path("data/papers"), help="Output path(default: data/papers)")
     download_parser.set_defaults(func=handle_download)
+
+    fetch_papers_parser = subparsers.add_parser("fetch-papers", help="Fetch papers from arXiv")
+    fetch_papers_parser.add_argument("query", type=str, help="Search query")
+    fetch_papers_parser.add_argument("--max-results", type=int, default=5, help="Maximum number of results (default: 5)")
+    fetch_papers_parser.add_argument("--output", type=str, default=Path("data/papers"), help="Output path(default: data/papers)")
+    fetch_papers_parser.set_defaults(func=handle_fetch_papers)
 
     args = parser.parse_args()
     args.func(args)
