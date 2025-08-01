1b30a95d07bbe4702be508dd9f23f884ead4d053
mortezai <morteza2006g@gmail.com>
2025-08-01 21:47:04 +0330

    refactor(integration): update arXiv downloader class

--- src/researcher/integrations/arxiv_downloader.py
+++ src/researcher/integrations/arxiv_downloader.py
@@ -4,9 +4,10 @@ from urllib.parse import urlparse
 
 
 class ArxivDownloader:
-    def __init__(self, download_dir: Path = Path("data/papers")):
+    def __init__(self, download_dir: Path = Path("data/papers"), print_msg: bool = False):
         self.download_dir = download_dir
         self.download_dir.mkdir(parents=True, exist_ok=True)
+        self.print_msg = print_msg
 
     def get_pdf_url(self, arxiv_abs_url: str) -> str:
         parsed = urlparse(arxiv_abs_url)
@@ -19,18 +20,18 @@ class ArxivDownloader:
         file_path = self.download_dir / f"{arxiv_id}.pdf"
 
         if file_path.exists():
-            print(f"✔ PDF already downloaded: {file_path}")
+            if self.print_msg: print(f"✔ PDF already downloaded: {file_path}");
             return file_path
 
         response = requests.get(pdf_url)
         if response.status_code == 200:
             file_path.write_bytes(response.content)
-            print(f"✅ Downloaded: {file_path}")
+            if self.print_msg: print(f"✅ Downloaded: {file_path}")
             return file_path
         else:
             raise Exception(f"Failed to download PDF: {pdf_url} | Status: {response.status_code}")
 
 if __name__ == "__main__":
-    downloader = ArxivDownloader()
+    downloader = ArxivDownloader(print_msg=True)
     pdf_path = downloader.download_pdf("https://arxiv.org/pdf/1706.03762")
     print(f"Saved to: {pdf_path}")
