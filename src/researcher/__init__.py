0d986beb1cec68b7d6a8bc156325db296e531389
mortezai <morteza2006g@gmail.com>
2025-08-01 18:06:20 +0330

    refactor(init): update init file and remove unnecessary part

--- src/researcher/__init__.py
+++ src/researcher/__init__.py
@@ -6,11 +6,9 @@ from .storage import FolderManager
 from .cli import main
 from .extract import PDFTextExtractor
 from .embedding import NomicEmbedder, simple_chunk, EmbeddingSearcher
-from .pipelines import FetchPapersPipeline
 
 __all__ = ["ArxivSearch", "ArxivDownloader", "FolderManager"
            , "PDFTextExtractor", "main"
            , "NomicEmbedder", "simple_chunk"
            , "EmbeddingSearcher"
-           , "FetchPapersPipeline"
 ]
