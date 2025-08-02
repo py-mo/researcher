afc9d022816177c69a46fd0c643f85862ba700e9
mortezai <morteza2006g@gmail.com>
2025-08-02 20:40:10 +0330

    chore(init): update pipeline init file by adding EmbedPaperPipeline

--- src/researcher/pipelines/__init__.py
+++ src/researcher/pipelines/__init__.py
@@ -1,4 +1,5 @@
 from .fetch_papers import FetchPapersPipeline
+from .embed_papers import EmbedPaperPipeline
 
 
-__all__ = ["FetchPapersPipeline"]
+__all__ = ["FetchPapersPipeline", "EmbedPaperPipeline"]
