b3c3c2b0428b02d50230174b44385352c05a05ed
mortezai <morteza2006g@gmail.com>
2025-08-03 20:18:19 +0330

    refactor(pipeline): add exception handling EmbedPaperPipeline

--- src/researcher/pipelines/embed_papers.py
+++ src/researcher/pipelines/embed_papers.py
@@ -10,13 +10,28 @@ class EmbedPaperPipeline:
         self.chunker = simple_chunk
         self.index_path = index_path
 
-    def run_pipeline(self):
-        pdf_files = list(self.pdf_dir.glob("*.pdf"))
+    def run_pipeline(self, raise_err: bool = False):
+        try:
+            pdf_files = list(self.pdf_dir.glob("*.pdf"))
+        except Exception as e:
+            if raise_err:
+                raise e
+            print(f"Fail to read files: {str(e)}"); return;
 
         for pdf in pdf_files:
-            text = PDFTextExtractor(pdf).extract_text()
-
-            if self.index_path:
-                self.embedder.embed_and_store(pdf, self.chunker(text)[0], self.index_path)
-            else:
-                self.embedder.embed_and_store(pdf, self.chunker(text), Path(f"data/embedding/{self.topic}"))
+            try:
+                text = PDFTextExtractor(pdf).extract_text()
+            except Exception as e:
+                if raise_err:
+                    raise e
+                print(f"Fail to extract text from {pdf}: {str(e)}"); continue;
+            
+            try:
+                if self.index_path != None:
+                    self.embedder.embed_and_store(pdf, self.chunker(text)[0], self.index_path)
+                else:
+                    self.embedder.embed_and_store(pdf, self.chunker(text), Path(f"data/embedding/{self.topic}"))
+            except Exception as e:
+                if raise_err:
+                    raise e
+                print(f"Fail to embed {pdf}: {str(e)}"); continue;
