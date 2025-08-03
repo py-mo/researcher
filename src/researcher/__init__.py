8a8ee6565c3215941bebce91cca97875e5f80ef0
mortezai <morteza2006g@gmail.com>
2025-08-03 19:36:46 +0330

    feat(pipeline&init): fix embedpaperpipeline and update init file

--- src/researcher/__init__.py
+++ src/researcher/__init__.py
@@ -3,12 +3,11 @@ __version__ = "0.1.0"
 from .integrations import ArxivSearch
 from .integrations import ArxivDownloader
 from .storage import FolderManager
-from .cli import main
 from .extract import PDFTextExtractor
 from .embedding import NomicEmbedder, simple_chunk, EmbeddingSearcher
 
 __all__ = ["ArxivSearch", "ArxivDownloader", "FolderManager"
-           , "PDFTextExtractor", "main"
+           , "PDFTextExtractor"
            , "NomicEmbedder", "simple_chunk"
            , "EmbeddingSearcher"
 ]
--- src/researcher/pipelines/embed_papers.py
+++ src/researcher/pipelines/embed_papers.py
@@ -12,13 +12,11 @@ class EmbedPaperPipeline:
 
     def run_pipeline(self):
         pdf_files = list(self.pdf_dir.glob("*.pdf"))
-        texts: str = ""
 
         for pdf in pdf_files:
             text = PDFTextExtractor(pdf).extract_text()
-            texts += text
 
-        if self.index_path:
-            self.embedder.embed_and_store(self.topic, self.chunker(texts)[0], self.index_path)
-        else:
-            self.embedder.embed_and_store(self.topic, self.chunker(texts), Path(f"data/embedding/{self.topic}"))
+            if self.index_path:
+                self.embedder.embed_and_store(pdf, self.chunker(text)[0], self.index_path)
+            else:
+                self.embedder.embed_and_store(pdf, self.chunker(text), Path(f"data/embedding/{self.topic}"))
