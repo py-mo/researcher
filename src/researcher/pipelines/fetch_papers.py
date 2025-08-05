b1f2963e0d13fffdfca471534cb31bbc229b0f8d
mortezai <morteza2006g@gmail.com>
2025-08-05 20:06:12 +0330

    refactor(pipeline): refactor fetch papers due to changes in arxiv downloader

--- src/researcher/pipelines/fetch_papers.py
+++ src/researcher/pipelines/fetch_papers.py
@@ -4,21 +4,26 @@ from pathlib import Path
 
 class FetchPapersPipeline:
     def __init__(self, query: str, max_results: int = 5
-                 , path: Path = None):
+                 , output_path: Path = None, metadata_path: Path = None):
         self.query = query
-        if (path != None) and (str(path) != "None"):
-             self.path = path
-        else:
-            self.path = Path(f"data/papers/{query}/")
         self.max_results = max_results
+        if (output_path != None) and (str(output_path) != "None"):
+             self.output_path = output_path
+        else:
+            self.output_path = Path(f"data/papers/{query}/")
+        if (metadata_path != None) and (str(metadata_path) != "None"):
+             self.metadata_path = metadata_path
+        else:
+            self.metadata_path = Path(f"data/metadata/{query}/")
 
     def run_pipeline(self, raise_err: bool = False):
         try:
             results: list = ArxivSearch(query=self.query, max_results=self.max_results).search()
-            downloader: ArxivDownloader = ArxivDownloader(download_dir=self.path)
+            downloader: ArxivDownloader = ArxivDownloader(download_dir=self.output_path
+                                                          , metadata_dir=self.metadata_path)
         except Exception as e:
             print(f"Failed to fetch papers.\n{str(e)}")
             if raise_err: raise e;
         else:
             for res in results:
-                downloader.download_pdf(res["link"])
+                downloader.download_pdf(res["link"], entries=res)
