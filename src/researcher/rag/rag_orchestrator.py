97f21a0fa5294807e60ba3b34526b7fc4a18ee9b
mortezai <morteza2006g@gmail.com>
2025-08-12 22:01:21 +0330

    refactor(rag): update rag orchestrator, remove unnecessary parts and fix context issue

--- src/researcher/rag/rag_orchestrator.py
+++ src/researcher/rag/rag_orchestrator.py
@@ -1,8 +1,7 @@
 from typing import Optional, List
-from pathlib import Path
 from researcher.pipelines import RetrieverPipeline
 from researcher import ( NomicEmbedder, LLMInference
-                        , FolderManager, PDFTextExtractor
+                        , FolderManager
                     )
 
 class RAGOrchestrator:
@@ -15,16 +14,10 @@ class RAGOrchestrator:
 
     def run(self, query: str, system_prompt: Optional[str] = None,
             options: Optional[dict] = None, k: Optional[int] = 5) -> str:
-        papers_ids: List[str] = self.retriever.run_pipeline(query=query, k_neighbors=k)
+        chunks: List[dict] = self.retriever.run_pipeline(query=query, k_neighbors=k)
 
-        docs:List[str] = []
-
-        for paper_id in papers_ids:
-            paper_path = self.fm.get_paper_path(paper_id=paper_id)
-            te = PDFTextExtractor(paper_path)
-            text = te.extract_text()
-            docs.append(text)
-
-        context = "\n\n".join(docs)
+        context = ""
+        for chunk in chunks:
+            context += "\n\n".join(chunk["chunk"])
         prompt = f"Use the following context to answer the question:\n\n{context}\n\nQuestion: {query}\nAnswer:"
         return self.llm.ask(prompt=prompt, system_prompt=system_prompt, options=options)
